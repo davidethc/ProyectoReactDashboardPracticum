6742863cf57b66359ce5fe0f4d1de73bd6a196ad
davidethc <davidecondet@gmail.com>
2025-07-12 16:44:32 -0500

    configuracion tailwind

--- Educate01/src/App.tsx
+++ Educate01/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import ReactDom from "react-dom/client";
+import React from "react";
+
+import Home from "./components/Home";
+
 function App() {
-  return <>s</>;
+  return (
+    <div className="bg-green-400 min-h-screen">
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/about" element={<h2>About Page</h2>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ Educate01/src/components/Home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div className="bg-red-300">holas</div>;
+}
