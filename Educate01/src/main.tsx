3f98f1c45ee18d40629bfa2b7a6add4ec61cca6a
davidethc <davidecondet@gmail.com>
2025-07-12 21:06:44 -0500

    is not valid reducerr

--- Educate01/src/main.tsx
+++ Educate01/src/main.tsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
 import { StrictMode } from "react";
 import "./index.css";
 
+import { store } from "./store/store";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
