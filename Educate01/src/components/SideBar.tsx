66671fa3fa4cb5373aec7cbc7dc61926b820dd0b
davidethc <davidecondet@gmail.com>
2025-07-12 21:52:35 -0500

    sideBarFin

--- Educate01/src/components/SideBar.tsx
+++ Educate01/src/components/SideBar.tsx
@@ -1,14 +1,26 @@
 import { FaAngleRight, FaApple } from "react-icons/fa";
 import { Link } from "react-router";
 
+import { closeSideBar } from "../store/sidabarSlice";
+import { useAppSelector } from "../store/store";
+import { useAppDispatch } from "../store/store";
+
 export const SideBar = () => {
+  const isSidebarOpen = useAppSelector((state) => state.sidebar.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <nav className="bg-red-300 w-[200px] min-h-screen flex flex-col">
+    <nav
+      className={`bg-red-300 w-[200px] min-h-screen flex flex-col fixed transition-all duration-300 ${
+        isSidebarOpen ? "left-0" : "-left-[200px]"
+      }`}
+    >
       <div className="h-14 flex items-center justify-center">
         <FaApple className="text-white text-4xl" />
       </div>
       <div className="flex flex-col p-4">
         <Link
+          onClick={() => dispatch(closeSideBar())}
           to={"#"}
           className="flex flex-row items-center text-white hover:bg-red-400 p-2 rounded transition-colors"
         >
@@ -16,6 +28,7 @@ export const SideBar = () => {
           <span className="ml-3 text-lg">Sistema</span>
         </Link>
         <Link
+          onClick={() => dispatch(closeSideBar())}
           to={"#"}
           className="flex flex-row items-center text-white hover:bg-red-400 p-2 rounded transition-colors mt-2"
         >
--- Educate01/src/pages/Home.tsx
+++ Educate01/src/pages/Home.tsx
@@ -1,14 +1,20 @@
 import { FaAlignJustify } from "react-icons/fa";
 
+import { openSideBar } from "../store/sidabarSlice";
 import { SideBar } from "../components/SideBar";
+import { useAppDispatch } from "../store/store";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
   return (
     <div className="min-h-screen flex">
       <SideBar />
       <div className="flex-1 flex flex-col">
         <header className="h-14 bg-blue-300 flex flex-row items-center px-4 relative">
-          <button className="text-white hover:bg-blue-500 p-2 rounded transition-colors">
+          <button
+            className="text-white hover:bg-blue-500 p-2 rounded transition-colors"
+            onClick={() => dispatch(openSideBar!())}
+          >
             <FaAlignJustify size={24} />
           </button>
           <h1 className="text-2xl text-white absolute left-1/2 -translate-x-1/2">
